c67196bdde7e5633df6dea60c088551a5a717e1b
Gowdaraju <57538893+Gowdaraju@users.noreply.github.com>
2020-09-09 18:34:38 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+//import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import { AuthContext } from "./context/auth";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PrivateRoute from "./PrivateRoute";
+
+function App(props) {
+  const existingTokens = JSON.parse(localStorage.getItem("tokens"));
+  const [authTokens, setAuthTokens] = useState(existingTokens);
+
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  };
+  return (
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home Page</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin Page</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <PrivateRoute path="/admin" component={Admin} />
+        </div>
+      </Router>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
